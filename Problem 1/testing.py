8a0fbd0830d266f73d395a9f5ae0ed540f111eb4
Tricky3d <111574146+Tricky3d@users.noreply.github.com>
2026-03-29 15:17:22 +0100

    Backend for Problem 1 done, no colour

--- Problem 1/testing.py
+++ Problem 1/testing.py
@@ -1,2 +1,75 @@
-print('hi')
-print('test')
+# Libraries
+
+import matplotlib.pyplot as plt
+import math
+import random
+
+
+# Input variables
+
+stepSize = float(input('Step size = '))
+stepCount = int(input('Number of steps =  '))
+
+
+# Intialise arrays
+
+xPos = 0.0
+yPos = 0.0
+xPath = [0] 
+yPath = [0]
+
+# Logic variables 
+
+theta = 0.0
+xMovement = 0.0
+yMovement = 0.0
+r = stepSize
+
+# 
+
+maximum = 0
+
+
+
+# Creating a 2d array 
+
+for i in range(1,stepCount + 1):
+
+    # Selects a random number between 0 and 1 and multiplies by 2π
+    theta = random.random() * (2 * math.pi)
+    
+    # x = r * cos θ
+    # y = r * sin θ 
+    xMovement = r * math.cos(theta)
+    yMovement = r * math.sin(theta)
+
+    # Adds the x and y coordinates to the current position
+    xPos += xMovement
+    yPos += yMovement
+
+    # Appends an array of [x,y] to the 2d array with all points on the path    
+    xPath.append(xPos)
+    yPath.append(yPos)
+
+maximum = max(abs(max(xPath, key=abs)), abs(max(yPath, key=abs)))
+
+
+fig, ax = plt.subplots(figsize=(8, 5))
+
+ax.set_xlim(-1 * maximum, maximum)
+ax.set_ylim(-1 * maximum, maximum)
+
+
+
+
+plt.plot(xPath, yPath)
+plt.show()
+
+
+
+
+
+    
+    
+
+    
