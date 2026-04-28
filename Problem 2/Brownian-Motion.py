d5694323aa6807e29b7eb927a35e0ef657ce7c2c
Tricky3d <111574146+Tricky3d@users.noreply.github.com>
2026-04-28 13:27:07 +0100

    sdfghjnm

--- Problem 2/Brownian-Motion.py
+++ Problem 2/Brownian-Motion.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import tkinter as tk
 from tkinter import simpledialog, messagebox
-
+import math
 
 # Graphical user interface
 
@@ -35,7 +35,7 @@ newPX = 0.0
 newPY = 0.0
 
 largePMass = 10.0
-largePRadius = 10
+largePRadius = 30
 
 bounds = 100
 
@@ -49,16 +49,17 @@ newPY = bounds/2
 
 for i in range(0,particleCount):
 
-    while bounds/2 - largePRadius*1.1 < newPX < bounds/2 + largePRadius*1.1:
+    while math.sqrt((newPX-bounds/2)**2 + (newPY-bounds/2)**2 ) < largePRadius:
         newPX = bounds * random.random()
-    
-    while bounds/2 - largePRadius*1.1 < newPY < bounds/2 + largePRadius*1.1:
         newPY = bounds * random.random()
 
     # 3rd value is the angle of the velocity (radians) will come in useful later
 
-    newParticle = [newPX, newPY, 0]
+    newParticle = [newPX, newPY]
     particles.append(newParticle)
+    
+    newPX = bounds/2
+    newPY = bounds/2
 
 
 # The fun stuff
